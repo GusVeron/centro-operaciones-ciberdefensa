b43ef1df8c9958ef9154ddb0b1b674a99f746776
Gustavo Veron <gusty-sgva@hotmail.com>
2026-07-17 19:06:40 -0300

    Clase Alerta y sus respectivos Tests

--- src/main/java/ar/edu/unahur/obj2/operaciones/Alerta.java
+++ src/main/java/ar/edu/unahur/obj2/operaciones/Alerta.java
@@ -1,9 +1,45 @@
 package ar.edu.unahur.obj2.operaciones;
 
-/**
- * Alerta
- */
 
 public class Alerta {
+
+    private String codigoIdentificador;
+    private String ipDeOrigen;
+    private Integer severidadBase;
+    private Long tiempoDeteccion;
+
+
+    public Alerta(String codigoIdentificador, String ipDeOrigen, Integer severidadBase, Long tiempoDeteccion) {
+
+        this.codigoIdentificador = codigoIdentificador;
+        this.ipDeOrigen = ipDeOrigen;
+        this.severidadBase = severidadBase;
+        this.tiempoDeteccion = tiempoDeteccion;
+    }
+
+
+
+    public String getCodigo() {
+        return codigoIdentificador;
+    }
+
+    public String getIpDeOrigen() {
+        return ipDeOrigen;
+    }
+
+    public Integer getSeveridadBase() {
+        return severidadBase;
+    }
+
+    public Long getTiempoDeteccion() {
+        return tiempoDeteccion;
+    }
+
+    // Metodo Booleano que me resuelve la problematica de si una alerta es critica o no!!.
+    public Boolean esCritica() {
+        return severidadBase > 90;
+    }
+    
+    
 
 }
--- src/test/java/ar/edu/unahur/obj2/operaciones/AlertaTest.java
+++ src/test/java/ar/edu/unahur/obj2/operaciones/AlertaTest.java
@@ -1,13 +1,47 @@
 package ar.edu.unahur.obj2.operaciones;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class AlertaTest {
+
+    // 1)
     @Test
-    public void unaAlertaSeCreaConSusValoresCorrectamente() {
-        assertTrue(true);
+    public void unaAlertaRecienCreadaConoceSuCodigo() {
+        
+        Alerta alerta1 = new Alerta("CVE-2024-123", "192.168.0.1", 80, 1400L);
+
+        assertEquals("CVE-2024-123", alerta1.getCodigo());
+    }
+
+    // 2)
+    @Test
+    public void unaAlertaRecienCreadaConoceSuSeveridadBase() {
+        
+        Alerta alerta1 = new Alerta("CVE-2024-123", "192.168.0.1", 80, 1400L);
+
+        assertEquals(80, alerta1.getSeveridadBase());
+    }
+
+    // 3) Aparece el primer comportamiento de la clase Alerta. Chequeo con assertFalse.
+    @Test
+    public void unaAlertaConSeveridadDeNoventaOMenosNoEsCritica() {
+        
+        Alerta alerta1 = new Alerta("CVE-2024-123", "192.168.0.1", 80, 1400L);
+        
+        assertFalse(alerta1.esCritica());
+    }
+    
+    // 4) Chequeo con assertTrue el caso borde.
+    @Test
+    public void unaAlertaConSeveridadDeMasDeNoventaEsCritica() {
+        
+        Alerta alerta1 = new Alerta("CVE-2024-123", "192.168.0.1", 91, 1400L);
+
+        assertTrue(alerta1.esCritica());
     }
 
 }
